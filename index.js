f9d0a77b4a3ecce8cafc370c8e6cb171660574b3
John Oduor Otieno <johnoduor222@gmail.com>
2022-08-25 13:39:44 -0700

    Done

--- index.js
+++ index.js
@@ -10,3 +10,45 @@ const messageConfig = {
       "Loki"
   ]
 };
+
+// 1. CLOSURE METHOD
+
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+
+//   const outerContext = this;
+
+//   this.signatories.forEach(function (signatory) {
+//     const message = `${outerContext.closing[signatory]}, ${signatory}`;
+//     console.log(message);
+//   });
+// };
+
+// printCard.call(messageConfig);
+
+// 2.thisArg METHOD
+
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+
+//   this.signatories.forEach(function (signatory) {
+//     const message = `${this.closing[signatory]}, ${signatory}`;
+//     console.log(message);
+//   }, this);
+// };
+
+// printCard.call(messageConfig);
+
+// 3.ARROW FUNCTION
+
+const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+  this.signatories.forEach((signatory) =>
+    console.log(`${this.closing[signatory]}, ${signatory}`)
+  );
+};
+
+printCard.call(messageConfig);
